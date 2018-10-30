93004977c1f11de00e01b45aa82ad7c301d6cfce
Kushniruk Viktor <vkushniruk@exadel.by>
2018-10-30 11:59:58 +0200

    check refresh token

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,9 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable, Request, UnauthorizedException } from '@nestjs/common';
-import * as jwt from 'jsonwebtoken';
+// import * as jwt from 'jsonwebtoken';
 import { Observable, of } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(
+        private readonly authService: AuthService,
+    ) { }
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
@@ -14,20 +18,26 @@ export class AuthGuard implements CanActivate {
     validateTokens(@Request() request) {
         // If token is in payload return true
         // else if refreshToken is the same as in DB get new tokens
-        let token: any;
         if (request.cookies.accessToken) {
-            token = jwt.decode(request.cookies.accessToken);
-            if (token) { return of(true); }
+            // token = jwt.decode(request.cookies.accessToken);
+            return of(true);
         }
         else {
+            // console.log('access token has expired', request);
             if (request.cookies.refreshToken) {
-
+                let tokenInDb;
+                if (request.cookies.refreshToken === tokenInDb) {
+                    return of(true);
+                } else { return of(false); }
             } else {
                 console.log('No refresh token');
 
             }
             throw new UnauthorizedException();
         }
+    }
+    getRefreshToken() {
+        // this.authService.getUserByUsername('userlooser').pipe(pluck('refreshToken'), tap(refreshToken => console.log('refreshToken', refreshTokenrefreshToken)))
     }
 
 }
